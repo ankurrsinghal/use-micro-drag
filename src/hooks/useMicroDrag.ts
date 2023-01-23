d30ec2ca9ba8c54499e881ad1df215e1ade63bcd
Ankur Singhal <ankursinghal@Ankurs-MacBook-Pro.local>
2023-01-24 00:49:57 +0530

    switched the PointerEvent to React synthetic event and passed the native event in the callback state

--- src/hooks/useMicroDrag.ts
+++ src/hooks/useMicroDrag.ts
@@ -7,7 +7,7 @@ export type DragCallbackState = {
   startY: number;
   dx: number;
   dy: number;
-  e: PointerEvent;
+  nativeEvent: PointerEvent;
 };
 
 export function useMicroDrag<T extends Element>(
@@ -17,7 +17,7 @@ export function useMicroDrag<T extends Element>(
   const isDraggingRef = useRef(false);
   const offsetRef = useRef([0, 0]);
 
-  const onPointerDown = useCallback((e: PointerEvent) => {
+  const onPointerDown = useCallback((e: React.PointerEvent) => {
     offsetRef.current = [e.clientX, e.clientY];
     isDraggingRef.current = true;
     ref.current?.setPointerCapture(e.pointerId);
@@ -26,13 +26,13 @@ export function useMicroDrag<T extends Element>(
       end: false,
       dx: 0,
       dy: 0,
-      e,
+      nativeEvent: e.nativeEvent,
       startX: e.clientX,
       startY: e.clientY,
     });
   }, []);
 
-  const onPointerMove = useCallback((e: PointerEvent) => {
+  const onPointerMove = useCallback((e: React.PointerEvent) => {
     if (isDraggingRef.current) {
       const dx = e.clientX - offsetRef.current[0];
       const dy = e.clientY - offsetRef.current[1];
@@ -41,14 +41,14 @@ export function useMicroDrag<T extends Element>(
         end: false,
         dx,
         dy,
-        e,
+        nativeEvent: e.nativeEvent,
         startX: offsetRef.current[0],
         startY: offsetRef.current[1],
       });
     }
   }, []);
 
-  const onPointerUp = useCallback((e: PointerEvent) => {
+  const onPointerUp = useCallback((e: React.PointerEvent) => {
     const dx = e.clientX - offsetRef.current[0];
     const dy = e.clientY - offsetRef.current[1];
     isDraggingRef.current = false;
@@ -58,7 +58,7 @@ export function useMicroDrag<T extends Element>(
       end: true,
       dx,
       dy,
-      e,
+      nativeEvent: e.nativeEvent,
       startX: offsetRef.current[0],
       startY: offsetRef.current[1],
     });
